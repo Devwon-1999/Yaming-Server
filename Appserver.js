79c69768934c12608c1ceb96087157932dfab08d
Devwon-1999 <Devwon.1999@gmail.com>
2023-10-21 01:47:25 +0900

    앱 서버 연동 구현

--- Appserver.js
+++ Appserver.js
@@ -1,48 +1,65 @@
+// //앱서버 관련
+// const express = require('express');
+// const mysql = require('mysql2');
+// const bodyParser = require('body-parser');
+// const router = express.Router();
+
+// // MySQL 데이터베이스 연결 설정
+// const db = mysql.createConnection({
+//   host: '34.168.81.56',
+//   user: 'root',
+//   database: 'yaming',
+//   password: 'qwer1234',
+//   port: '5000',
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
+
+// db.connect(err => {
+//   if (err) {
+//     console.error('MySQL connection error:', err);
+//   } else {
+//     console.log('Connected to MySQL database');
+//   }
+// });
+
+// // 라우트 설정
+// router.get('/api/login', (req, res) => {
+//   const { email, password } = req.body;
+
+//   // 여기서 MySQL 데이터베이스를 쿼리하여 인증 로직을 수행
+//   // 결과에 따라 응답을 생성
+
+//   // 예시: 인증 성공 시 토큰을 생성하고 전송
+//   if (email === 'user@example.com' && password === 'password123') {
+//     const token = 'your_access_token'; 
+//     res.json({ success: true, token });
+//   } else {
+//     res.status(401).json({ success: false, message: 'Authentication failed' });
+//   }
+// });
+
+// module.exports = router;
+
 //앱서버 관련
 const express = require('express');
-const mysql = require('mysql');
-const bodyParser = require('body-parser');
-
-const app = express();
-const port = 3000;
-
-// MySQL 데이터베이스 연결 설정
-const db = mysql.createConnection({
-  host: '34.168.81.56',
-  user: 'root',
-  database: 'yaming',
-  password: 'qwer1234'
-});
-
-db.connect(err => {
-  if (err) {
-    console.error('MySQL connection error:', err);
-  } else {
-    console.log('Connected to MySQL database');
-  }
-});
-
-// 미들웨어 설정
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+const router = express.Router();
 
 // 라우트 설정
-app.post('/login', (req, res) => {
-  const { email, password } = req.body;
+router.post('/user', (req, res) => {
+  //var email = getPrameter("email");
+  //var password = getPrameter("password");
+  //&& password === 'password123'
 
-  // 여기서 MySQL 데이터베이스를 쿼리하여 인증 로직을 수행
-  // 결과에 따라 응답을 생성
-
-  // 예시: 인증 성공 시 토큰을 생성하고 전송
-  if (email === 'user@example.com' && password === 'password123') {
-    const token = 'your_access_token';
+  if (req.body.email === 'user@example.com') {
+    const token = 'your_access_token'; 
+    console.log('login success');
     res.json({ success: true, token });
   } else {
     res.status(401).json({ success: false, message: 'Authentication failed' });
+    console.log('login fail');
   }
 });
 
-// 서버 시작
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+module.exports = router;
--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 // 서버 메인 코드
 //const express = require('필요한 모듈명')
 //npm run start (서버 시작)
+
 const express = require('express');
 const userRouter = require('./userRouter');
+const Appserver = require('./Appserver')
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.json());
+
 app.use(`/user`, userRouter);
+app.use('/login', Appserver);
 
 app.get('/', (req, res) => {
     res.json({
